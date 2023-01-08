283b90cb4442ab6aa9ef6a1e91b82221af435513
armenh1991 <armenhek@gmail.com>
2023-01-08 11:15:58 -0600

    Initial

---
+++ brain.py
@@ -0,0 +1,13 @@
+# def xcord(event):
+#     global xcord
+#     xcord = int(event.x)
+#     print(xcord)
+#
+#
+# def ycord(event):
+#     global ycord
+#     ycord = int(event.y)
+#     print(ycord)
+#
+#
+#
---
+++ ui.py
@@ -0,0 +1,20 @@
+# from tkinter import *
+# from brain import *
+# from PIL import Image, ImageTk
+#
+# THEME_COLOR = "#ECE8DD"
+#
+# class TicTacToe:
+#
+#     def __init__(self):
+#         self.window = Tk()
+#         self.window.title("Tic Tac Toe")
+#         self.canvas = Canvas(width=500, height=500, bg=THEME_COLOR, highlightthickness=0)
+#         self.window.config(padx=20, pady=20, highlightthickness=0)
+#         self.canvas.grid(row=0, column=0,)
+#         self.picture = PhotoImage(file="images/tic.png")
+#         self.canvas.create_image(250, 250, image=self.picture)
+#         self.button_1 = Button()
+#         self.button_1.grid(row=0, column=0)
+#
+#         self.window.mainloop()
